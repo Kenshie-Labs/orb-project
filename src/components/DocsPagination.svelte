b2635f2661be9cd644aa7157611df2a6434c713d
axel <nshxel@gmail.com>
2026-05-30 10:40:03 +0700

    fix: overhauling the pagination.svete feature, which previously had long text overflow, to be more proportional

--- src/components/DocsPagination.svelte
+++ src/components/DocsPagination.svelte
@@ -4,22 +4,32 @@
   export let nextPage: { title: string; url: string } | null;
 </script>
 
-<nav class="flex justify-between items-center py-8">
+<nav class="flex justify-between items-start py-8"> <!-- Changed items-center to items-start -->
   {#if prevPage}
-    <a href={prevPage.url} class="flex items-center space-x-2 text-primary-500 hover:text-primary-700 transition-colors duration-200">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <a href={prevPage.url} class="flex items-start text-primary-500
+      hover:text-orange-500 active:text-orange-500
+      transition-colors duration-200 max-w-[48%] group text-left">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 shrink-0 mr-2 mt-1" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
       </svg>
-      <span>{prevPage.title}</span>
+      <div class="flex flex-col flex-1 min-w-0">
+        <span class="text-xs text-gray-500 dark:text-gray-400">Previous</span>
+        <span class="font-semibold leading-tight">{prevPage.title}</span>
+      </div>
     </a>
   {:else}
     <span />
   {/if}
 
   {#if nextPage}
-    <a href={nextPage.url} class="flex items-center space-x-2 text-primary-500 hover:text-primary-700 transition-colors duration-200">
-      <span>{nextPage.title}</span>
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <a href={nextPage.url} class="flex items-start justify-end text-primary-500
+      hover:text-orange-500 active:text-orange-500
+      transition-colors duration-200 max-w-[48%] group text-right">
+      <div class="flex flex-col flex-1 min-w-0">
+        <span class="text-xs text-gray-500 dark:text-gray-400">Next</span>
+        <span class="font-semibold leading-tight">{nextPage.title}</span>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 shrink-0 ml-2 mt-1" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
       </svg>
     </a>
